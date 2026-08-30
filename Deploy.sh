0f718ec3fcc04618f7cba8038adfa36a2a35e367
TeaJhay <TeaJhay@outlook.com.au>
2026-08-30 22:22:54 +1000

    Dont care to comment

--- Deploy.sh
+++ Deploy.sh
@@ -17,6 +17,8 @@ cd /mnt
 sudo nixos-install
 
 echo "== Creating pristine @void-blank snapshot =="
+
+sleep 5
 sudo umount -R /mnt
 sudo mount -o rw,subvol=/ /dev/disk/by-partlabel/disk-main-root /mnt
 sudo btrfs subvolume snapshot -r /mnt/@void /mnt/@void-blank
